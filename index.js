3209e0d7c4b484802a92f1fead461a51e1d347e2
nepalankit <nepalankit16@gmail.com>
2023-04-17 16:13:11 +0545

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbName: "backend",
+  })
+  .then(() => console.log("DataBase Connected"))
+  .catch((e) => console.log(e));
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Messge = mongoose.model("message", messageSchema);
+
 const app = express();
 const user = [];
 //using middleware
@@ -7,8 +23,13 @@ app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
 //setting up view engine
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index", { Name: "bankit" });
+
+app.get("/add", async (req, res) => {
+  await Messge.create({ name: "Ankit", email: "sample2@gmail.com" }).then(
+    () => {
+      res.send("nice");
+    }
+  );
 });
 
 app.get("/success", (req, res) => {
@@ -27,6 +48,10 @@ app.get("/user", (req, res) => {
   res.json({
     user,
   });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(5000, () => {
